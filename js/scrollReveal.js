ec858815ceb4ec115905dd694ca2b4c15c8b77f6
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-03-09 17:41:38 +0100

    [ANIM]ScrollReveal header+about+portfolio+hire

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -1,8 +1,26 @@
 console.log("coucou");
 
-ScrollReveal().reveal('.photo-profil', { delay: 200 });
+//Header
+ScrollReveal().reveal('.photo-profil');
 ScrollReveal().reveal('.tagline', { delay: 500 });
 ScrollReveal().reveal('.punchline', { delay: 1000 });
 ScrollReveal().reveal('.type', { delay: 1500 });
 ScrollReveal().reveal('.CTA', { delay: 2500 });
 
+//Hero
+ScrollReveal().reveal('.hero-content', { distance: '50%', duration: 2500 });
+
+//Services
+ScrollReveal().reveal('.services', { distance: '50%', duration: 2500 });
+
+//Portfolio
+ScrollReveal({ distance: '100%', interval: 500, opacity: 0.4});
+ScrollReveal().reveal('.blueasy', { origin: 'left', easing: 'ease'});
+ScrollReveal().reveal('.sokoban', { origin: 'right', easing: 'ease' });
+ScrollReveal().reveal('.vinyles', { origin: 'right', easing: 'ease' });
+ScrollReveal().reveal('.lambda', { origin: 'left', easing: 'ease' });
+ScrollReveal().reveal('.festi_france', { origin: 'right', easing: 'ease' });
+ScrollReveal().reveal('.discri', { origin: 'left', easing: 'ease' });
+
+//Hire
+ScrollReveal().reveal('.logo-itfy', { delay: 500, opacity: 0, easing: 'ease-in' });
