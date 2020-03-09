2ec0f1fe7c3751ffba35d53932e1ad19711511a4
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-03-09 16:47:00 +0100

    [NEW]LibrairieJS ScrollReveal

---
+++ js/scrollReveal.js
@@ -0,0 +1,8 @@
+console.log("coucou");
+
+ScrollReveal().reveal('.photo-profil', { delay: 200 });
+ScrollReveal().reveal('.tagline', { delay: 500 });
+ScrollReveal().reveal('.punchline', { delay: 1000 });
+ScrollReveal().reveal('.type', { delay: 1500 });
+ScrollReveal().reveal('.CTA', { delay: 2500 });
+
